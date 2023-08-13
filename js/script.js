115d92d681958ceaf93f8fe38f281cf450425163
Adelto Almeida <adtgutiar@gmail.com>
2023-08-12 22:54:24 -0300

    Criando a funcao moveSnake(),dentro da funcao, vamos pegarmos sempre a ultima posicao da array, no caso a snake, a ultima posicao sempre vai ser a cabeca head. vamos atribuir em uma variavel head, para podermos ter referencia a cabeca, e criaremos mais uma variavel, vamos chamar de position, para podermos aplicar as posicoes, atraves de condicoes que iremos atribuir a funcao.

--- js/script.js
+++ js/script.js
@@ -2,19 +2,37 @@ const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
 const size = 30
+let direction = ""
 
 const snake = [
   {x : 200, y : 200},
   {x : 230, y : 200},
-  {x : 260, y : 200},
-  {x : 290, y : 200}
+  {x : 260, y : 200}
 ]
 
 const drawSnake = () => {
   ctx.fillStyle = '#ddd'
-
-  snake.forEach((position) => {
+  snake.forEach((position, index) => {
+    if(index == snake.length - 1){
+      ctx.fillStyle = '#aaa'
+    }
     ctx.fillRect(position.x, position.y, size, size)
   })
 }
-drawSnake()
+
+const moveSnake = () => {
+
+  if(!direction) return
+  const head = snake[snake.length - 1]
+
+  if(direction == 'right') {
+    snake.push({ x : head.x + size , y : head.y })
+  }
+  snake.shift()
+}
+
+setInterval(() => {
+  ctx.clearRect(0, 0, 600, 600)
+  moveSnake()
+  drawSnake()
+}, 300);
